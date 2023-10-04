c57aeda73741f796c59cd6f45f86f75e838654f0
MChassanNajib <chassannajib@gmail.com>
2023-10-04 10:13:53 +0700

    fix : page

--- code/script.js
+++ code/script.js
@@ -2,10 +2,8 @@ const navbarMenu = document.querySelector(".navbar .links");
 const hamburgerBtn = document.querySelector(".hamburger-btn");
 const hideMenuBtn = navbarMenu.querySelector(".close-btn");
 
-// Show mobile menu
 hamburgerBtn.addEventListener("click", () => {
     navbarMenu.classList.toggle("show-menu");
 });
 
-// Hide mobile menu
 hideMenuBtn.addEventListener("click", () =>  hamburgerBtn.click());
